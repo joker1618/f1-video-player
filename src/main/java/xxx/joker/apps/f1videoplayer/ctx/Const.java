6931b2e71938a145d19b03497f571994219a7bb4
joker1618 <federicobarbano@gmail.com>
2019-09-27 23:58:30 +0100

    fix

--- src/main/java/xxx/joker/apps/f1videoplayer/ctx/Const.java
+++ src/main/java/xxx/joker/apps/f1videoplayer/ctx/Const.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class Const {
 
     private static final Path BASE_FOLDER = JkEnvironment.getAppsFolder();
-    public static final Path REPO_FOLDER = BASE_FOLDER.resolve("f1-video-player-last");
+    public static final Path REPO_FOLDER = BASE_FOLDER.resolve("f1-video-player-repo");
     public static final String DB_NAME = "vp";
 
     public static AtomicBoolean showScenicView = new AtomicBoolean(false);
